807cd8b0497a841ffa86724b14740bb2d6ee50b8
Shin seongsu <tlstjdtn321@naver.com>
2024-03-24 02:08:05 +0900

    refactor: LazyLoading관련 일을 Loader쪽에서 지지않도록 수정

--- src/main/java/persistence/sql/entity/loader/EntityLoaderImpl.java
+++ src/main/java/persistence/sql/entity/loader/EntityLoaderImpl.java
@@ -9,7 +9,6 @@ import persistence.sql.dml.query.builder.SelectQueryBuilder;
 import persistence.sql.dml.query.clause.ColumnClause;
 import persistence.sql.dml.query.clause.WhereClause;
 import persistence.sql.entity.EntityMappingTable;
-import persistence.sql.entity.collection.LazyLoadingManager;
 import persistence.sql.entity.model.PrimaryDomainType;
 
 import java.util.Collections;
@@ -21,18 +20,15 @@ public class EntityLoaderImpl implements EntityLoader {
     private final EntityLoaderMapper entityLoaderMapper;
     private final SelectQueryBuilder selectQueryBuilder;
     private final EagerSelectQueryBuilder eagerSelectQueryBuilder;
-    private final LazyLoadingManager lazyLoadingManager;
 
     public EntityLoaderImpl(final JdbcTemplate jdbcTemplate,
                             final EntityLoaderMapper entityLoaderMapper,
                             final SelectQueryBuilder selectQueryBuilder,
-                            final EagerSelectQueryBuilder eagerSelectQueryBuilder,
-                            final LazyLoadingManager lazyLoadingManager) {
+                            final EagerSelectQueryBuilder eagerSelectQueryBuilder) {
         this.jdbcTemplate = jdbcTemplate;
         this.entityLoaderMapper = entityLoaderMapper;
         this.selectQueryBuilder = selectQueryBuilder;
         this.eagerSelectQueryBuilder = eagerSelectQueryBuilder;
-        this.lazyLoadingManager = lazyLoadingManager;
     }
 
     @Override
@@ -66,16 +62,7 @@ public class EntityLoaderImpl implements EntityLoader {
                 columnClause,
                 whereClause);
 
-        if (entityMappingTable.hasFetchType(FetchType.LAZY)) {
-            return getLazyLoading(sql, clazz);
-        }
-
         return jdbcTemplate.queryForObject(sql, resultSet -> entityLoaderMapper.mapper(clazz, resultSet));
-    }
-
-    private <T> T getLazyLoading(final String sql, final Class<T> clazz) {
-        T entity = jdbcTemplate.queryForObject(sql, resultSet -> entityLoaderMapper.mapper(clazz, resultSet));
-        return lazyLoadingManager.setLazyLoading(entity);
     }
 
     private <T> T getEagerTypeSql(final EntityMappingTable entityMappingTable,
