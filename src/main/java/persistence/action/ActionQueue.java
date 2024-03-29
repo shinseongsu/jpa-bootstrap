07afdc8a0b041f8df3ca103338e7c0cdc85da78f
신성수 <tlstjdtn321@naver.com>
2024-03-29 15:06:13 +0900

    feat: ActionQUeue 기능 추가

---
+++ src/main/java/persistence/action/ActionQueue.java
@@ -0,0 +1,14 @@
+package persistence.action;
+
+public interface ActionQueue {
+
+    void addInsertAction(InsertEntityAction<?> insertEntityAction);
+
+    Object excuteWithReturnKey(InsertEntityAction<?> insertEntityAction);
+
+    void addUpdateAction(UpdateEntityAction<?> updateEntityAction);
+
+    void addDeleteAction(DeleteEntityAction<?> deleteEntityAction);
+
+    void allExecute();
+}
---
+++ src/main/java/persistence/action/ActionQueueImpl.java
@@ -0,0 +1,68 @@
+package persistence.action;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class ActionQueueImpl implements ActionQueue {
+
+    private final Queue<InsertEntityAction<?>> insertEntityActionQueue;
+    private final Queue<UpdateEntityAction<?>> updateEntityActionQueue;
+    private final Queue<DeleteEntityAction<?>> deleteEntityActionQueue;
+
+    public ActionQueueImpl() {
+        this.insertEntityActionQueue = new ConcurrentLinkedQueue<>();
+        this.updateEntityActionQueue = new ConcurrentLinkedQueue<>();
+        this.deleteEntityActionQueue = new ConcurrentLinkedQueue<>();
+    }
+
+    @Override
+    public void addInsertAction(final InsertEntityAction<?> insertEntityAction) {
+        if(insertEntityActionQueue.contains(insertEntityAction)) {
+            return;
+        }
+        insertEntityActionQueue.add(insertEntityAction);
+    }
+
+    @Override
+    public Object excuteWithReturnKey(InsertEntityAction<?> insertEntityAction) {
+        insertEntityActionQueue.add(insertEntityAction);
+        return executeInsert();
+    }
+
+    @Override
+    public void addUpdateAction(final UpdateEntityAction<?> updateEntityAction) {
+        updateEntityActionQueue.add(updateEntityAction);
+    }
+
+    @Override
+    public void addDeleteAction(final DeleteEntityAction<?> deleteEntityAction) {
+        deleteEntityActionQueue.add(deleteEntityAction);
+    }
+
+    @Override
+    public void allExecute() {
+        executeInsert();
+        executeUdpate();
+        executeDelete();
+    }
+
+    private Object executeInsert() {
+        Object key = null;
+        while(!insertEntityActionQueue.isEmpty()) {
+            key = insertEntityActionQueue.poll().executeWithReturn();
+        }
+        return key;
+    }
+
+    private void executeUdpate() {
+        while(!updateEntityActionQueue.isEmpty()) {
+            updateEntityActionQueue.poll().execute();
+        }
+    }
+
+    private void executeDelete() {
+        while(!deleteEntityActionQueue.isEmpty()) {
+            deleteEntityActionQueue.poll().execute();
+        }
+    }
+}
