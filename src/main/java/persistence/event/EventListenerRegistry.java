79c51bb8c0a22286cb263c429e70d64cf6712478
신성수 <tlstjdtn321@naver.com>
2024-03-29 15:05:20 +0900

    feat: EventType 반환 하여 기능할 수 있도록 하는 Factory 추가

---
+++ src/main/java/persistence/event/EventListenerRegistry.java
@@ -0,0 +1,56 @@
+package persistence.event;
+
+import persistence.action.ActionQueue;
+import persistence.event.delete.DefaultDeleteEventListener;
+import persistence.event.delete.DeleteEventListener;
+import persistence.event.load.DefaultLoadEventListener;
+import persistence.event.load.LoadEventListener;
+import persistence.event.merge.DefaultMergeEventListener;
+import persistence.event.merge.MergeEventListener;
+import persistence.event.persist.DefaultPersisterEventListener;
+import persistence.event.persist.PersisterEventListener;
+import persistence.metadata.MetaModel;
+
+public class EventListenerRegistry {
+
+    private final LoadEventListener loadEventListener;
+    private final MergeEventListener mergeEventListener;
+    private final DeleteEventListener deleteEventListener;
+    private final PersisterEventListener persisterEventListener;
+
+
+    private EventListenerRegistry(final LoadEventListener loadEventListener,
+                                  final MergeEventListener mergeEventListener,
+                                  final DeleteEventListener deleteEventListener,
+                                  final PersisterEventListener persisterEventListener) {
+        this.loadEventListener = loadEventListener;
+        this.mergeEventListener = mergeEventListener;
+        this.deleteEventListener = deleteEventListener;
+        this.persisterEventListener = persisterEventListener;
+    }
+
+    public static EventListenerRegistry create(final MetaModel metaModel, final ActionQueue actionQueue) {
+        return new EventListenerRegistry(
+                new DefaultLoadEventListener(metaModel),
+                new DefaultMergeEventListener(metaModel, actionQueue),
+                new DefaultDeleteEventListener(metaModel, actionQueue),
+                new DefaultPersisterEventListener(metaModel, actionQueue)
+        );
+    }
+
+    public LoadEventListener getLoadEventListener() {
+        return loadEventListener;
+    }
+
+    public MergeEventListener getMergeEventListener() {
+        return mergeEventListener;
+    }
+
+    public DeleteEventListener getDeleteEventListener() {
+        return deleteEventListener;
+    }
+
+    public PersisterEventListener getPersisterEventListener() {
+        return persisterEventListener;
+    }
+}
